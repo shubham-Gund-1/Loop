d24456720c3d46e9a59694a6c312ef553e4db285
shubham-Gund-1 <shubhamgund3480@gmail.com>
2024-05-28 18:35:52 +0530

    Loop

--- Mult.java
+++ Mult.java
@@ -1,1 +1,16 @@
 // 13) Multiplication table using + operator
+import java.util.Scanner;
+public class Mult{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        int number = scanner.nextInt();
+        for (int i = 1; i <= 10; i++) {
+            int result = 0;
+            for (int j = 0; j < i; j++) {
+                result=result+number;
+            }
+            System.out.println(number + " * " + i + " = " + result);
+        }
+    }
+}
--- Name.java
+++ Name.java
@@ -1,13 +1,14 @@
 // 15)Write a program to read your name and print it ‘n’ times
-import java.util.*;
+import java.util.Scanner;
 public class Name{
-    public static void mian(String args[]){
-        Scanner input=new Scanner(System.in);
-        String n;
-        System.out.println("Enter Your Name:");
-        n=input.nextString();
-        for(int i=1;i<10;i++){
-            System.out.print(" "+n);
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
+        System.out.print("Enter the number of times to print your name: ");
+        int n = scanner.nextInt();
+        for (int i = 0; i < n; i++) {
+            System.out.println(name);
         }
     }
 }
